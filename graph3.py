b903d94dc95acebd6aa1c3709497bcc34aae2d09
Aleksis.kevins <aleksis.kevins@gmail.com>
2023-09-11 13:42:25 +0300

    ss

--- graph3.py
+++ graph3.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 xpoints = np.array([1, 2, 6, 8, 6, 6, 3])
-ypoints = np.array([3, 8, 1, 10, 1, 6, 4])
+ypoints = np.array([3, 7, 1, 10, 1, 6, 4])
 
 plt.plot(xpoints, marker = 'o')
 plt.show()
