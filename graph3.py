b0779d112c6aec1c6bb4a484f701f93d58b7392e
Aleksis.kevins <aleksis.kevins@gmail.com>
2023-09-11 13:23:15 +0300

    ej dirsts

---
+++ graph3.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8, 6, 6, 3])
+ypoints = np.array([3, 8, 1, 10, 1, 6, 4])
+
+plt.plot(xpoints, marker = 'o')
+plt.show()
+
+
